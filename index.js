fedc49de4e3ec5a32b56e0d85eb204f3289b7d3e
mani <mani95lisa@gmail.com>
2014-11-04 22:00:39 +0800

    change regexp for 0.5.3

--- index.js
+++ index.js
@@ -3,23 +3,21 @@
     var Audio = {};
 
     Audio.parse = function(postContent, callback) {
-        var re = /\[audio\](.*)\[\/audio\]/gm;
+        var re = /\[audio\](.*)\s.*\s.*\[\/audio\]/gm;
         if (re.test(postContent) !== false) {
             var arr = postContent.match(re);
             var audioString = arr[0];
             re = /href="(.*?)">(.*?)</gm;
             arr = audioString.match(re);
             var result = '<ul id="playlist" style="list-style-type: none;padding: 0;">';
-            if(arr){
-                arr.forEach(function(value){
-                    var url = value.replace(re, '$1');
-                    var name = value.replace(re, '$2');
-                    result += '<li data-link="'+url+'"><i class="play-btn"></i>'+name+'</li>'
-                });
-                result+='</ul>';
-                re = /\[audio\](.*)\[\/audio\]/gm;
-                postContent = postContent.replace(re, result);
-            }
+            arr.forEach(function(value){
+                var url = value.replace(re, '$1');
+                var name = value.replace(re, '$2');
+                result += '<li data-link="'+url+'"><i class="play-btn"></i>'+name+'</li>'
+            });
+            result+='</ul>';
+            re = /\[audio\](.*)\s.*\s.*\[\/audio\]/gm;
+            postContent = postContent.replace(re, result);
         }
         callback(null, postContent);
     };
